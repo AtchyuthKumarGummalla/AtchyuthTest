01d8b5145d724645288097dd12547004fa4dcfc2
bandana choudhury <bandana.munu@gmail.com>
2019-03-11 18:19:45 +0530

    added

--- AtchyuthTestGit/ViewController.swift
+++ AtchyuthTestGit/ViewController.swift
@@ -22,11 +22,12 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        print("dmfbdhfbd")
+        return UITableViewCell()
     }
     
     
