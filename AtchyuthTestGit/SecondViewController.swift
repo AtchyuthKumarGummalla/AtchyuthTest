86da4e25230b497013085fdb66f7f3ac558a11fb
Atchyuth <atchyuth4@gmail.com>
2019-03-11 11:10:16 +0530

    Added SecondViewController

---
+++ AtchyuthTestGit/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  AtchyuthTestGit
+//
+//  Created by Mobeen on 11/03/19.
+//  Copyright © 2019 Mobeen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       print("Hiiii")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
