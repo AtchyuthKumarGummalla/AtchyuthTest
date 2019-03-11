171185ee7c83be27078368a4bd57cbd5043bea39
bandana choudhury <bandana.munu@gmail.com>
2019-03-11 18:09:51 +0530

    added

--- AtchyuthTestGit/ViewController.swift
+++ AtchyuthTestGit/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     
        print("dfn", separator: "cdjfnm", terminator: "dmf")
+        print("dfn", separator: "cdjfnm", terminator: "dmf")
+
         
     }
 }
