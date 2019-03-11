5a6f668698d8cfa8f3e4f23bc5986ac5a40a34e8
bandana choudhury <bandana.munu@gmail.com>
2019-03-11 18:00:59 +0530

    added new scree

---
+++ AtchyuthTestGit/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  AtchyuthTestGit
+//
+//  Created by Ravi on 3/11/19.
+//  Copyright © 2019 Mobeen. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+print("jdfndj")    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
